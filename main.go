c9cf1e1a800a7341f5825bd0a252bc9c243030ce
ebarsulai <ebarsulai@gmail.com>
2024-06-25 14:01:26 +0300

    Refactor server routing and add POST handling
    Refactored server routing to handle POST requests and added index.html serving.

--- main.go
+++ main.go
@@ -9,28 +9,23 @@ import (
 )
 
 func main() {
-
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Switch on the requested URL path
 		switch r.URL.Path {
 		case "/":
-			
-			if r.Method == http.MethodPost{
-				server.HandlePostRequest(w,r)
-			}else{
+
+			if r.Method == http.MethodPost {
+				server.HandlePostRequest(w, r)
+			} else {
 				http.ServeFile(w, r, "templates/index.html") // Serve the index.html file located in the templates directory
 			}
 
-		
-				
-
 		default:
-			
+
 			http.NotFound(w, r) // Respond with a 404 Not Found status for all other paths
 		}
 	})
-	
+
 	// The server runs asynchronously on port 8080 using a goroutine.
 	go func() {
 		if err := http.ListenAndServe(":8080", nil); err != nil {
