784e175376e11e74677a35a304c3471573468876
abrakingoo <maikoabraham68@gmail.com>
2024-06-25 12:13:07 +0300

    Refactor: URL path handling and home page response logic
    Restructured URL path handling in http.HandleFunc to improve readability and maintainability.
    Updated asciiweb package's HandleHome function to enhance clarity and consistency in serving the home page.

--- main.go
+++ main.go
@@ -4,9 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	server "asciiweb/server"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Switch on the requested URL path
+		switch r.URL.Path {
+		case "/":
+			
+			server.HandleHome(w, r) // Handle the root path by calling the HandleHome function
+		default:
+			
+			http.NotFound(w, r) // Respond with a 404 Not Found status for all other paths
+		}
+	})
+	
 	// The server runs asynchronously on port 8080 using a goroutine.
 	go func() {
 		if err := http.ListenAndServe(":8080", nil); err != nil {
