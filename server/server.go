4f5d5280ac2b13f468fb9e60170bdb15ac8bdd2d
ebarsulai <ebarsulai@gmail.com>
2024-06-25 14:02:55 +0300

    Refactor: Add POST request handling for ASCII art generation
    Implemented HandlePostRequest function to process user input and generate ASCII art based on selected banner style.

--- server/server.go
+++ server/server.go
@@ -1,10 +1,25 @@
 package asciiweb
 
 import (
-	"fmt"
+	art "asciiweb/server/ascii-art"
+	// "fmt"
 	"net/http"
+	"html/template"
 )
 
-func HandlePostRequest(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Welco")
+type Result struct {
+	Data string
+}
+func HandlePostRequest(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	userInput := r.FormValue("text")
+	bannerFile := r.FormValue("banner")
+
+	artOutput := Result{
+		Data: art.GenArt(userInput, bannerFile) ,
+		}
+
+	tpl, _:= template.ParseFiles("templates/index.html")
+
+	tpl.Execute(w, artOutput)
 }
