1de0c3e88e12108ca1a56382f45c68599d918303
ebarsulai <ebarsulai@gmail.com>
2024-07-06 12:58:51 +0300

    Refactor: Add file existence check for index.html
    Implemented a checkFileExists() function to verify if templates/index.html exists.

--- main.go
+++ main.go
@@ -4,11 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	server "asciiweb/server"
 )
 
+func checkFileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		// Handle other errors if needed
+		fmt.Println("Error checking file:", err)
+		return false
+	}
+	return true
+}
+
 func main() {
+	indexFilePath := "templates/index.html"
+
+	// Check if the index.html file exists
+	if !checkFileExists(indexFilePath) {
+		log.Fatalf("File %s does not exist.", indexFilePath)
+		return
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Switch on the requested URL path
 		switch r.URL.Path {
