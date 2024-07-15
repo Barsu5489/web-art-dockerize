d1e28ca18c0357bae2216d20a9c55ccf18bd9c41
abrakingoo <maikoabraham68@gmail.com>
2024-07-15 15:24:28 +0300

    Feat: the scripts helps in cleaning of the unused object

---
+++ cleaner.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+# Remove all stopped containers
+docker container prune -f 
+
+# Remove all unused images
+docker image prune -a -f 
+
+# Remove all unused volumes
+docker volume prune -f 
+
+# Remove all unused networks
+docker network prune -f
+
